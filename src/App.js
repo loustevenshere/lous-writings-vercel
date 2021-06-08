769fe9749c8d6f9886bdb76183f60775d5c47fa7
Louis Stevens <loustevenshere@gmail.com>
2021-06-08 14:43:08 -0400

    Started styling

--- src/App.js
+++ src/App.js
@@ -6,12 +6,12 @@ import PoemList from './Components/PoemList';
 function App() {
  
   return (
-    <>
+    <div>
 <h1 className="title">Welcome</h1>
 <p>This is a place where I can store my writings</p>
 <p>My name is Louis Stevens and I hope you enjoy</p>
 <PoemList />
-    </>
+    </div>
   );
 }
 
--- src/Components/Poem.js
+++ src/Components/Poem.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import '../Styling/Poem.css'
 
 const Poem = ({poem}) => {
     return (
-        <>
+        <div>
         <h1>{poem.title}</h1>
         <p>{poem.text}</p>
-        </>
+        </div>
     )
 }
 
