4892ce94021c53d3cc54d8b8a1ed4d48c55a5450
Louis Stevens <loustevenshere@gmail.com>
2021-06-09 11:48:05 -0400

    Poem header onclick functions working

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { useEffect, useState } from 'react';
 import './App.css';
 import PoemList from './Components/PoemList';
 
@@ -7,9 +6,10 @@ function App() {
  
   return (
     <div>
-<h1 className="title">Welcome</h1>
+<h1>Welcome</h1>
 <p>This is a place where I can store my writings</p>
 <p>My name is Louis Stevens and I hope you enjoy</p>
+
 <PoemList />
     </div>
   );
--- src/Components/Poem.js
+++ src/Components/Poem.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../Styling/Poem.css'
 
 const Poem = ({poem}) => {
+    const [click, setclick] = useState(false);
+
+    const handleClick = () => {
+        setclick(!click)
+    }
+
+
     return (
         <div>
-        <h1>{poem.title}</h1>
+        <h1 onClick={handleClick}>{poem.title}</h1>
+        {click ? 
         <p>{poem.text}</p>
+         : null}
+
         </div>
     )
 }
