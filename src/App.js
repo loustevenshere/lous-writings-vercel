445c189df86ecd2165aad46ab791cc4370f0c0f0
Louis Stevens <loustevenshere@gmail.com>
2021-06-10 13:28:52 -0400

    making progress

--- src/App.js
+++ src/App.js
@@ -5,13 +5,18 @@ import PoemList from './Components/PoemList';
 function App() {
  
   return (
-    <div>
-<h1>Welcome</h1>
-<p>This is a place where I can store my writings</p>
-<p>My name is Louis Stevens and I hope you enjoy</p>
-
+    <>
+    <div className="welcome_container">
+<h1 className="welcome">Welcome</h1>
+<p className="welcome_writings">This is a place where I can store my writings</p>
+{/* <p>My name is Louis Stevens and I hope you enjoy</p> */}
+  </div>
+  <div className="poems_list">
 <PoemList />
-    </div>
+  </div>
+</>
+  
+
   );
 }
 
